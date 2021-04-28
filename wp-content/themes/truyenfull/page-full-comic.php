cdc54a7c259c8ad564b38bd1d71ced3c961ada8d
Arsen Truong <arsen.truong@contemi.com.vn>
2021-04-28 13:56:21 +0700

    test effect

--- wp-content/themes/truyenfull/page-full-comic.php
+++ wp-content/themes/truyenfull/page-full-comic.php
@@ -30,7 +30,7 @@ $args = array(
 	<div class="row">
 		<div class="col-md-9 content">
 							
-			<section class="section">
+			<section class="section test-effect">
 				<header class="section-header">
 					<h2><?php the_title(); ?></h2>
 				</header>
